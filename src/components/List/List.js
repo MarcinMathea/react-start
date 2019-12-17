85554f0124e580d7ff3809f4b28f94e9d9ea8d69
Marcin Mathea <mar.mathea@gmail.com>
2019-12-17 23:48:40 +0100

    added columns problem in props

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import Column from '../Column/Column.js';
+import PropTypes from 'prop-types';
+
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+  }
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} src={this.props.src} />
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
+        <div className={styles.column}>
+          <Column headerText={this.props.headerText} />
+        </div>
       </section>
     )
   }
