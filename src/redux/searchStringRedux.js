1a2cd46b04255634e05b4cbb97392bb376c9a724
Marcin Mathea <mar.mathea@gmail.com>
2020-01-24 17:15:07 +0100

    search problem

---
+++ src/redux/searchStringRedux.js
@@ -0,0 +1,22 @@
+import shortid from 'shortid';
+// selectors
+export const getSearchString = ({searchString}) =>  searchString;
+export const countAllCards = ({cards}) => cards.length;
+export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
+// action name creator
+const reducerName = 'search';
+const createActionName = name => `app/${reducerName}/${name}`;
+// actions types
+export const CHANGE = createActionName('CHANGE');
+// action creators
+export const changeSearchString = payload => ({ payload: { ...payload, id: shortid.generate() }, type: CHANGE });
+// reducer
+export default function reducer(statePart = '', action = {}) {
+  switch (action.type) {
+    case CHANGE:
+      return [...statePart, action.payload];
+    default:
+      return statePart;
+  }
+}
+
