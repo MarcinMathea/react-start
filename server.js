fc24048ddeb42f852feb6397b9a4306dbe513bab
Marcin Mathea <mar.mathea@gmail.com>
2020-04-20 20:01:59 +0200

    heroku problem

---
+++ server.js
@@ -0,0 +1,14 @@
+
+const httpServer = require('http-server');
+const server = httpServer.create();
+const router = httpServer.router('dist/db/app.json');
+const middlewares = httpServer.defaults({
+  static: 'dist',
+  noCors: true,
+});
+const port = process.env.PORT || 3131;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
